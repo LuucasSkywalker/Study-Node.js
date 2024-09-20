ec1481dd5084dfab036a24487ef6bf915891a6e5
Lucas Cavalcante de Rezende <luucasskywalker@gmail.com>
2024-09-20 00:55:05 -0300

    funcionarios pasta e depedencias

--- node/ModuloCliente.js
+++ node/ModuloCliente.js
@@ -1,8 +1,8 @@
 
 
-const moduloA = require('./ModuloA')  /* aqui estamos usando o require, para importar
+const moduloA = require('./moduloA')  /* aqui estamos usando o require, para importar
 algo do ('./ModuloA') . Iremos ter o objeto do ModuloA . Porque eu requiro uma
 exportação do ModuloA, necessido de algo dentro do ModuloA*/
-const ModuloB =  require("./ModuloB")
+const moduloB =  require("./moduloB")
 console.log(moduloA.bemVindo)
 console.log(moduloA)
---
+++ node/pastaA/pastaB/acessaModulo.js
@@ -0,0 +1,4 @@
+// saindo da pasta "b" e saindo da pasta "a" para acessa past
+// para sair de uma pasta usasse "../"
+const moduloB = require('../../moduloB')
+console.log(moduloB)
